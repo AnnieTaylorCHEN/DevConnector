be9dd17f2df5a85cde54d451a0c66884879944b6
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-07-25 18:17:12 +0200

    add redux and set up alert.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,40 +1,29 @@
 import React, {Fragment, useState } from 'react'
-import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { setAlert } from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
         password2: '' 
     })
+    const { name, email, password, password2 } = formData
+
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
     const onSubmit = async e => {
         e.preventDefault()
         if (password !== password2) {
-            console.log('Passwords do not match.')
+            setAlert('Passwords do not match.', 'danger')
         } else {
-            const newUser = {
-                name, 
-                email,
-                password
-            }
-            try {
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }
-                const body = JSON.stringify(newUser)
-                const res = await axios.post('/api/users', body, config)
-                console.log(res.data)
-            } catch (error) {
-                console.error(error.response.data)
-            }
+            console.log('Success')
         }
     }
 
-    const { name, email, password, password2 } = formData
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -90,10 +79,14 @@ const Register = () => {
                 />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     )
 }
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+}
+
+export default connect(null, {setAlert})(Register)
