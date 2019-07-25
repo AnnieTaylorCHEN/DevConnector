376f664e1bec4c8b5b22f4a42d68dc191c4a09a9
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-07-25 14:45:23 +0200

    add router of register and login, test register with axios.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
     return (
@@ -11,8 +12,8 @@ const Landing = () => {
                         other developers
                     </p>
                     <div className="buttons">
-                        <a href="register.html" class="btn btn-primary">Sign Up</a>
-                        <a href="login.html" class="btn btn-light">Login</a>
+                        <Link to="/register" class="btn btn-primary">Sign Up</Link>
+                        <Link to="/login" class="btn btn-light">Login</Link>
                     </div>
                 </div>
             </div>
