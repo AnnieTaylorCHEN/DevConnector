087cae481e56bc65a86f553e4ce7a502de3cf1a2
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-07-26 19:36:30 +0200

    finish login, logout, navbar links.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './types'
+import { 
+    REGISTER_SUCCESS, 
+    REGISTER_FAIL, 
+    USER_LOADED, 
+    AUTH_ERROR, 
+    LOGIN_SUCCESS, 
+    LOGIN_FAIL, 
+    LOGOUT
+} from './types'
 import setAuthToken from '../utils/setAuthToken'
 
 //load user
@@ -47,4 +55,36 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_FAIL
         })
     }
+}
+
+//login user
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+          'Content-Type' : 'application/json'
+        } 
+    }
+
+    const body = JSON.stringify({ email, password })
+
+    try {
+        const res = await axios.post('/api/auth', body, config)
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+    } catch (error) {
+        const errors = error.response.data.errors
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+        dispatch({
+            type: LOGIN_FAIL
+        })
+    }
+}
+
+//logout /clear profile
+export const logout = () => dispatch => {
+    dispatch({type: LOGOUT })
 }
